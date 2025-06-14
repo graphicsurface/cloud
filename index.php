c957c082d250265eecaf3cdac1ff654500694d26
Abdul Musabbir <126709118+abdul-musabbir@users.noreply.github.com>
2025-06-15 02:21:39 +0600

    upload the all images

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$directory = 'End_of_Course_Geometry';
+
+$files = scandir($directory);
+$files = array_diff($files, array('.', '..', '.DS_Store'));
+
+foreach ($files as $file) {
+    // Match files like download.png or download-1.png
+    if (preg_match('/^download(?:-(\d+))?\.png$/', $file, $matches)) {
+        $number = isset($matches[1]) ? (int)$matches[1] : 0; // If no number, it's download.png → 0
+        $newName = $number . '.png';
+        $oldPath = $directory . '/' . $file;
+        $newPath = $directory . '/' . $newName;
+
+        // Rename the file
+        if (rename($oldPath, $newPath)) {
+            echo "Renamed $file → $newName\n";
+        } else {
+            echo "Failed to rename $file\n";
+        }
+    }
+}
